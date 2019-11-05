c52c41946c38849c44a41056e8d228a14561c4e5
beasts-festivals1 <54179837+beasts-festivals1@users.noreply.github.com>
2019-11-05 18:29:37 +0000

    Test

--- bot.js
+++ bot.js
@@ -16,53 +16,20 @@ bot.on('message', message => {
 
     let args = message.content.substring(PREFIX.length).split(" ");
 
-    switch (args[0]) {
-        case 'play':
-
-            function play(connection, message){
-                var server = servers[message.guild.id];
-
-                server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
-
-                server.queue.shift();
-
-                server.dispatcher.on("end", function(){
-                    if(server.queue[0]){
-                        play(connection, message);
-                    }else {
-                        connection.disconnect();
-                    }
-                })
-            }
-
-            if(!args[1]){
-                message.channel.send("Please provide a valid link!");
-                return;
-            }
-
-            if(!message.member.voiceChannel){
-                message.channel.send("Please join the music channel to have access to play music!")
-                return;
-            }
-
-            if(!servers[message.guild.id]) servers[message.guild.id] = {
-                queue: []
-            }
-
-            var server = servers[message.guild.id];
-
-            server.queue.push(args[1]);
-
-            if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
-                play(connection, message);
-            })
-
-
-
-
-
-
-        break;
+    if(message.content.startsWith('$help')){
+        const helpembed = new discord.RichEmbed()  
+        helpembed.setTitle('Help Page!')
+        helpembed.addField('$hamachi', 'Displays the Hamachi Network Name and Password');
+        helpembed.addField('$ip','Shows you the current IP to log on');
+        helpembed.addField('$help', 'Shows the help page');  
+        helpembed.addField('$staff', 'Shows you the current Staff Team');
+        helpembed.addField('$trailer', 'Sends you the link to watch the official Minor Citizens trailer');
+        helpembed.addField('$mods', 'Sends you the link to download all the mods for Minor Citizens');
+        helpembed.addField('$profile', 'Views your profile');
+        helpembed.addField('$updates', 'View the latest updates for Minor Citizens')
+        helpembed.setFooter('For staff commands type $shelp');
+        helpembed.setColor(0xFFC300)
+        message.channel.send(helpembed)
     }
 })
 
