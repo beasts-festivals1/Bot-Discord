cf31e5dc8fd3d99008fcdaaedf21b1df5d858f71
beasts-festivals1 <54179837+beasts-festivals1@users.noreply.github.com>
2019-11-05 17:08:44 +0000

    Test

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login('NjQwNjgwNDAyNTgzMDI3NzEz.Xb9aBw.zlWM2mYuAEuXDzpcCm8QgoHRu_4')
+
+client.on('ready', ()=>{
+    console.log('Bot on!');
+})
+
